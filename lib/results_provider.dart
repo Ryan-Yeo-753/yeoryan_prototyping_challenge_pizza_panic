40a065211994125a79f787805dcf65af985bc572
Ryan_Yeo_753 <stmallworldforever@gmail.com>
2025-11-22 14:16:45 -0800

    ILLEGAL SUBMISSION
    re-commit

--- lib/results_provider.dart
+++ lib/results_provider.dart
@@ -53,7 +53,7 @@ class DataSavings extends Notifier<ScoutingData> {
       additionalData: state.additionalData
     );
 
-    final client = await Nyxx.connectGateway('MTQ0MTkwMDU2MTA3NDIyNTIyMw.GubUcM.DEBNwoOmF_-7dLgpUm1kSjE3ubHT8-iB-ubUtM',
+    final client = await Nyxx.connectGateway('',
       GatewayIntents.allUnprivileged,
     );
 
